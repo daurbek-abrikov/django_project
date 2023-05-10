764281214d92b4665b4884ca1e46ed1a711a230e
Daurbek <daurbek.abrikov@gmail.com>
2023-05-10 16:28:11 +0600

    - Added pagination for home page, so it shows 6 posts per page - Now we can visit profile page of users, which shows some info about them and posts by this author - If it is your profile page, besides that, you can see 'edit profile' button - So form for editing your profile page is on separate page (not in profile page straightly) which feels more natural

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.forms.models import BaseModelForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
@@ -16,6 +16,17 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # since by default it would be: '<app>/<model>_<view_type>.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 6
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' # since by default it would be: '<app>/<model>_<view_type>.html'
+    context_object_name = 'posts'
+    paginate_by = 6
+    
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
